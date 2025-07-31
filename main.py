4151bd299be89b0faeed0c22550108b74d8b68f6
Andres Cuervo <andresmauro17@gmail.com>
2025-07-31 15:10:19 -0500

    preparing to store the dataabase

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 DB_FILE_NAME = 'database.db'
 
-def migrate_database()  :
+def migrate_database()-> bool:
     """ this method is used to migrate the database in sqlite3 """
     logger.info('Starting database migration')
     if os.path.exists(DB_FILE_NAME):
         os.remove(DB_FILE_NAME)
-    return None
+    conn = None
     try:
         conn = sqlite3.connect(DB_FILE_NAME)
         cursor = conn.cursor()
@@ -33,17 +33,21 @@ def migrate_database()  :
         ''')
         conn.commit()
         logger.info('Database migration completed successfully')
-        return None
+        return True
     except sqlite3.Error as e:
         logger.error(f"An error occurred during database migration: {e}")
+        return False
+    finally:
         if conn:
             conn.close()
             logger.info('Database connection closed')
-        return None
 
-def get_coheficientes() :
+
+def get_coheficientes() -> list[tuple] | None:
     """ this method is used to get the coefficients from the csv file """
-    coheficientes = []  
+    logger.info('Starting to process the CSV file')
+    conn = sqlite3.connect(DB_FILE_NAME)
+    records_to_insert: list[tuple] = []
 
     with open('import_test.csv', mode='r') as file:
         csv_reader = csv.reader(file, delimiter=';')
@@ -51,16 +55,24 @@ def get_coheficientes() :
             # print(index)
             if( index != 0):
                 # print(row)
-                name = row[1]
-                score = float(row[3])   
-                coheficiente= name.split(' ')[1]
+                id: int= int(row[0]) 
+                name: str = row[1]
+                brand: str = row[2]
+                score: float = float(row[3])
+                coheficiente: str | float = name.split(' ')[1]
                 coheficiente = float(coheficiente.replace(',', '.'))
+                
+                # print(f'id: {id}, name: {name}, brand: {brand}, score: {score}, coheficiente: {coheficiente}')
 
-                print(type(score), type(coheficiente))
-                probability = score * coheficiente
-                print(probability)
+
+                probability: float = score * coheficiente
+                records_to_insert.append((id, name, brand, score, coheficiente, probability))
+    return records_to_insert
 
 
 if __name__ == "__main__":
-    migrate_database()
-    # get_coheficientes()
+    was_migrated: bool = migrate_database()
+    if was_migrated:
+        records_to_insert: list[tuple] | None = get_coheficientes()
+        print(records_to_insert)
+        
